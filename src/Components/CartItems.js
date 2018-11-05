1d462218eb64650b8af95839c7703a734596191d
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-11-05 11:56:49 -0800

    created list

---
+++ src/Components/CartItems.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import CartItem from './CartItem'
+
+const CartItems = ({cartItemsList}) => {
+    let list = cartItemsList.map(item => 
+        <CartItem product={item.product.name} price={item.product.priceInCents} quantity={item.quantity}/>
+    )
+    return (
+        <div className="container">
+            <h1>Cart Items</h1>
+            <div className="list-group">
+                <div className="list-group-item">
+                <div className="row">
+                    <div className="col-md-8">Product</div>
+                    <div className="col-md-2">Price</div>
+                    <div className="col-md-2">Quantity</div>
+                </div>
+                </div>
+                {/* Items go here */}
+                {list}
+            </div>
+        </div>
+    )
+
+}
+
+export default CartItems
